adef3a2fc9b3f509d36d42efa504d6489cc405cb
Syre <chestersyron.damian14@outlook.com>
2022-03-27 02:18:33 +0800

    Init Commit - Setup Tailwind - Created Hero Page

---
+++ static/tailwind.config.js
@@ -0,0 +1,20 @@
+module.exports = {
+  content: [
+    '../static/**/*.{html, js}',
+    '../templates/**/*.{html, js}',
+  ],
+  theme: {
+    colors:{
+      'body': '#FDD935',
+      'matte-black': '#1C1C1C',
+      'white': '#F1F1F1',
+      'btn-theme':'#1C1C1C',
+      'selected-text': '#114084'
+    },
+
+    fontFamily: {
+      poppins: ['Poppins', 'Sans-Serif'],
+    },
+  },
+  plugins: [],
+}
